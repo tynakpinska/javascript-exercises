5ff761b6e1021a24dea313e2512679443de47c28
tynakpinska <m.k.kepinska@gmail.com>
2022-08-26 16:39:41 +0200

    do exercises 9-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = people => {
+  people.sort((person1, person2) => {
+    if (!person1.yearOfDeath) {
+      return new Date().getFullYear() - person1.yearOfBirth >
+        person2.yearOfDeath - person2.yearOfBirth
+        ? -1
+        : 1;
+    }
+    if (!person2.yearOfDeath) {
+      return person1.yearOfDeath - person1.yearOfBirth >
+        new Date().getFullYear() - person2.yearOfBirth
+        ? -1
+        : 1;
+    }
+    return person1.yearOfDeath - person1.yearOfBirth >
+      person2.yearOfDeath - person2.yearOfBirth
+      ? -1
+      : 1;
+  });
+  return people[0];
 };
 
 // Do not edit below this line
